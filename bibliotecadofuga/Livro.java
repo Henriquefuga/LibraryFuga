10441e8f9806e360ba68f488daf5c904f64a6362
Henrique Fuga Gomes <111388305+Henriquefuga@users.noreply.github.com>
2023-09-21 11:31:27 -0300

    Add files via upload

---
+++ bibliotecadofuga/Livro.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bibliotecadofuga;
+
+/**
+ *
+ * @author henri
+ */
+public class Livro {
+    private String nome;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
